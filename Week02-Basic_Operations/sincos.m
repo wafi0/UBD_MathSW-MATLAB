688a4be125b0fac9dba71c46fb0f659114607bbc
wafi0 <110456392+wafi0@users.noreply.github.com>
2022-08-09 20:49:29 +0800

    First commit!
    Directly added on GitHub as I'm sure if it's possible to commit to an empty repository from MATLAB

---
+++ Week02-Basic_Operations/sincos.m
@@ -0,0 +1,19 @@
+%% Script for Sin and Cosine plots
+
+clear             % Clear workspace
+clc               % Clear command log
+
+x = 0:pi/100:4*pi;  % Vector for x coordinates
+y = sin(x);         % Vector for sin x
+y2 = cos(x);        % Vector for cos x
+
+plot(x,y, "k")                  % "k" corresponds to black line
+hold on                         % plot the next curve on the same graph
+plot(x,y2,"--r","LineWidth",2)  % "--r" corresponds to dashed red line; width of 2
+
+xlabel("x")          % add axis label
+ylabel("y")          % add axis label
+axis([0 4*pi, -1 1]) % set coordinate range
+
+title("Plot of sin and cosine functions", "FontSize", 12)
+legend("sin(x)","cos(x)")
